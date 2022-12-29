9f4cb15ae30ac3ac1c796004d0852a2eaaaceba9
julhas <julhas040@gmail.com>
2022-12-29 13:26:15 +0600

    dragon-news-servesite

---
+++ index.js
@@ -0,0 +1,22 @@
+const { request } = require("express");
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require("./data/category.json");
+
+app.get("/", (req, res) => {
+  res.send("News API Running");
+});
+
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.listen(port, () => {
+  console.log("Dragon News Server running on port");
+});
